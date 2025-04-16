db384180f2e7d11ab72f5ac9fcbca4fa28e45fc2
jaehun306 <jaehun306@naver.com>
2025-04-16 17:08:31 +0900

    @Configuration과 싱글톤, 바이트 코드 조작 컴포넌트 스캔과 의존관계 자동 주입

---
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,47 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    @DisplayName("")
+    void configurationTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+        MemberRepository memberRepository = ac.getBean(MemberRepository.class);
+
+        System.out.println("memberService -> memberRepository1 = " + memberRepository1);
+        System.out.println("orderService -> memberRepository2 = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        assertThat(memberRepository1).isSameAs(memberRepository);
+        assertThat(memberRepository2).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig appConfig = ac.getBean(AppConfig.class);
+        /**
+         * 스프링이 CGLIB이라는 바이트코드를 조작하는 라이브러리를 사용해서 AppConfig의 클래스를 상속받은
+         * 임의의 다른 클래스를 만들고, 그 다른 클래스를 스프링 빈으로 등록한다.
+         */
+        System.out.println("appConfig = " + appConfig);
+        System.out.println("appConfig = " + appConfig.getClass());
+    }
+}
