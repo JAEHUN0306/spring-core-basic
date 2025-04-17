768f6f581c30f177501c4469488c2d40acec91d2
jaehun306 <jaehun306@naver.com>
2025-04-17 17:16:42 +0900

    조회 빈이 2개 이상인 경우 - 해결

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,9 +2,13 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Qualifier("mainDiscount")
+@Primary // 3. 조회 빈이 두개 이상인 경우 - @Primary 사용 (권장)
 public class RateDiscountPolicy implements DiscountPolicy {
     private int discountRate = 10;
 
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,6 +6,7 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,7 +14,15 @@ import org.springframework.stereotype.Component;
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+    //private final DiscountPolicy rateDiscountPolicy; 1.조회 빈이 두개 이상인 경우 - 같은 필드 명으로 주입
 
+    /*
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("fixDiscountPolicy") DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    } 2. 조회 빈이 두개 이상인 경우 - @Qualifire 사용, 구분자를 추가해주는 역할
+    */
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
