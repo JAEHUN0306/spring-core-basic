cc42f90bf2befd568755d1b43aa78910ef249762
jaehun306 <jaehun306@naver.com>
2025-04-18 16:59:08 +0900

    빈 생명주기 콜백 - 빈 등록 초기화, 소멸 메서드

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -19,7 +19,7 @@ public class BeanLifeCycleTest {
 
     @Configuration
     static class LifeCycleConfig {
-        @Bean
+        @Bean(initMethod = "init") // destroyMethod = (inferred)
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
